79da92513a84657353564007edb79f9361236960
Dimple Kumar <44132154+DKumar0001@users.noreply.github.com>
2020-11-25 16:36:19 +0530

    Create Hour_Glass_Sum.py

---
+++ Recursion/HackerRank/Hour_Glass_Sum.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    max_sum = -10000
+    current_sum = 0
+    length = 6
+    width = 6
+    for i in range(1,5):
+        for j in range(1,5):
+            current_sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]+ arr[i][j] + arr[i+1][j-1] + arr[i+1][j] +arr[i+1][j+1]
+            if(current_sum>max_sum):
+                max_sum = current_sum
+    return max_sum
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
