efaa3f167d3e8370a638090f705e70835e91b941
Dimple Kumar <44132154+DKumar0001@users.noreply.github.com>
2020-11-25 16:39:16 +0530

    Update Hour_Glass_Sum.py

--- Recursion/HackerRank/Hour_Glass_Sum.py
+++ Recursion/HackerRank/Hour_Glass_Sum.py
@@ -12,10 +12,10 @@ def hourglassSum(arr):
     current_sum = 0
     length = 6
     width = 6
-    for i in range(1,5):
-        for j in range(1,5):
+    for i in range(1, length-1):
+        for j in range(1, width-1):
             current_sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]+ arr[i][j] + arr[i+1][j-1] + arr[i+1][j] +arr[i+1][j+1]
-            if(current_sum>max_sum):
+            if(current_sum > max_sum):
                 max_sum = current_sum
     return max_sum
     
